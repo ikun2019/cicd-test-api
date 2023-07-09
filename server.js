11729148083c586bb024ba930687764a7501698a
ikun <0215.miyatto@gmail.com>
2023-07-09 23:25:04 +0900

    create todo

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/db');
+const Todo = require('./models/Todo');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
+app.get('/api', async (req, res, next) => {
+  try {
+    const todos = await Todo.findAll();
+    res.status(200).json({
+      success: true,
+      todos: todos
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post('/api', async (req, res, next) => {
+  try {
+    const todo = new Todo({
+      title: req.body.todo
+    });
+    await todo.save();
+    res.status(200).json({
+      success: true
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 
 sequelize
